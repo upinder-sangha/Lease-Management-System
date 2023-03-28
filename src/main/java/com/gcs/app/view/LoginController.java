ef255716bb5a321d71f4f3a657f0ba25dd3f02e6
nilesh1168 <n.suryawanshi1168@gmail.com>
2023-03-28 16:29:12 -0400

    added login functionality

---
+++ src/main/java/com/gcs/app/view/LoginController.java
@@ -0,0 +1,69 @@
+package com.gcs.app.view;
+
+import java.io.IOException;
+
+import com.gcs.app.session.Session;
+import com.gcs.app.tasks.LoginTask;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class LoginController {
+
+	@FXML
+	TextField usernameField;
+
+	@FXML
+	PasswordField passwordField;
+
+	@FXML
+	Button loginButton;
+
+	@FXML
+	Label errorLabel;
+		
+	@FXML
+	public void onLoginBtnClick() {
+		String username = usernameField.getText();
+		String password = passwordField.getText();
+		errorLabel.setText("");
+
+		if (username.isBlank() || password.isBlank()) {
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("Err");
+			alert.setHeaderText("Login Error");
+			alert.setContentText("Username/Password is blank!");
+			alert.show();
+		} else {
+			LoginTask loginTask = new LoginTask(username, password);
+			loginTask.messageProperty().addListener((observable, oldValue, newValue) -> {
+					errorLabel.setText(newValue);
+			});
+			loginTask.valueProperty().addListener((observable, oldValue, newValue) -> {
+				if(newValue!=null) {
+					// get a handle to the stage
+				    Stage stage = (Stage) loginButton.getScene().getWindow();
+				    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("admin-view.fxml"));
+				    try {
+				    	Scene scene = new Scene(fxmlLoader.load(), 700, 400);
+				    	stage.setScene(scene);
+					} catch (IOException e) {
+						System.out.println(e.getMessage());
+					}
+				}
+			});
+			Thread loginThread = new Thread(loginTask);
+			loginThread.setDaemon(true);
+			loginThread.start();
+		}
+	}
+
+}
