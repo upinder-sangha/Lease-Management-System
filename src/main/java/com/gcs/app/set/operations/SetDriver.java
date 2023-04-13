d9eb9f47ac43ee13f94d9ef8f8cebdb2f4ca6f60
nilesh1168 <n.suryawanshi1168@gmail.com>
2023-04-13 10:51:50 -0400

    minor changes to generic code

--- src/main/java/com/gcs/app/set/operations/SetDriver.java
+++ src/main/java/com/gcs/app/set/operations/SetDriver.java
@@ -3,9 +3,9 @@ package com.gcs.app.set.operations;
 public class SetDriver {
 	public static void main(String[] args) {
 		GenericDataClass obj1 = new GenericDataClass("Test 1", 1);
-		GenericDataClass obj2 = new GenericDataClass("Test 2", 2);
-		GenericDataClass obj3 = new GenericDataClass("Test 3", 3);
-		GenericDataClass obj4 = new GenericDataClass("Test 4", 2);
+		GenericDataClass obj2 = new GenericDataClass(1, 2);
+		GenericDataClass obj3 = new GenericDataClass(1.1, 3);
+		GenericDataClass obj4 = new GenericDataClass(true, 2); // Cannot add requested item to Set: Existing ID!!
 		GenericSet set = new GenericSet();
 		set.add(obj1);
 		set.add(obj2);
